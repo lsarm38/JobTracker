535568adf5c85396549f131dc470a76d88bcddef
lsarm38 <lsarm38@gmail.com>
2026-06-28 23:19:32 -0500

    Created the DbContext, wired up Program

---
+++ JobTracker.API/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using JobTracker.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobTracker.API.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Application> Applications => Set<Application>();
+    public DbSet<Note> Notes => Set<Note>();
+    public DbSet<StatusHistory> StatusHistories => Set<StatusHistory>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Application>()
+            .HasMany(a => a.Notes)
+            .WithOne(n => n.Application)
+            .HasForeignKey(n => n.ApplicationId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Application>()
+            .HasMany(a => a.StatusHistories)
+            .WithOne(s => s.Application)
+            .HasForeignKey(s => s.ApplicationId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
--- JobTracker.API/Program.cs
+++ JobTracker.API/Program.cs
@@ -1,17 +1,32 @@
+using JobTracker.API.Data;
+using JobTracker.API.Services;
+using JobTracker.API.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+builder.Services.AddScoped<IApplicationService, ApplicationService>();
+builder.Services.AddScoped<INoteService, NoteService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors("AllowAll");
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
